b336c57afeda624908d4549368614c91ee486f96
lucalugie <118959975+lucalugie@users.noreply.github.com>
2023-10-04 12:10:11 +0700

    test

--- func_test1.js
+++ func_test1.js
@@ -1,28 +1,22 @@
 const { helloworld, one_plus } = require("./Utils");
 
-if(helloworld() === "hello world")
-{
-    console.log("hello world");
-
-}else{
-    console.log(1);
-    return;
+if (helloworld() === "hello world") {
+} else {
+  console.log(1);
+  return;
 }
 
-//test case: one_plus 
-if(one_plus(1) === 3) //oneplus <--- 1 ==> 2
-{
-    
-}else{
-    console.log(1);
-    return;
+//test case: one_plus
+if (one_plus(1) === 3) {
+  //oneplus <--- 1 ==> 2
+} else {
+  console.log(1);
+  return;
 }
 
-
-if(one_plus(2) === 4) //oneplus <--- 1 ==> 2
-{
-    
-}else{
-    console.log(1);
-    return;
+if (one_plus(2) === 4) {
+  //oneplus <--- 1 ==> 2
+} else {
+  console.log(1);
+  return;
 }
