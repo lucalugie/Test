aa77703f65eef83dceb49938ad1da9804daf1a44
lucalugie <118959975+lucalugie@users.noreply.github.com>
2023-10-05 09:43:05 +0700

    Update func_test1.js

--- func_test1.js
+++ func_test1.js
@@ -1,22 +1,30 @@
+const fs = require("fs");
+
 const { helloworld, one_plus } = require("./Utils");
+
+let str = "";
+let is_error = 0;
 
 if (helloworld() === "hello world") {
 } else {
-  console.log(1);
-  return;
+  str = str + "error at: helloworld(): at 1\n";
+  is_error = 1;
 }
 
 //test case: one_plus
 if (one_plus(1) === 3) {
   //oneplus <--- 1 ==> 2
 } else {
-  console.log(1);
-  return;
+  str = str + "error at: one_plus(1): at 1\n";
+  is_error = 1;
 }
 
 if (one_plus(2) === 4) {
   //oneplus <--- 1 ==> 2
 } else {
-  console.log(1);
-  return;
+  str = str + "error at: one_plus(2): at 2\n";
+  is_error = 1;
 }
+
+fs.writeFileSync("log.txt", str);
+console.log(is_error);
